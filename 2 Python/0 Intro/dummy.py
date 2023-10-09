550fce0d6d4b0029664f66f9fed58789eb7439c2
Ravi Kumar <56071550+ravikumar1302@users.noreply.github.com>
2023-10-09 23:02:21 +0530

    9-10-23 Python Libraries added

--- 2 Python/0 Intro/dummy.py
+++ 2 Python/0 Intro/dummy.py
@@ -1,14 +1,13 @@
+import numpy as np
 
 print("Hello")
 
 
-i=80
-k=1
-while i%2 ==0:
-    print('{} is Even no'.format(i))
-    print(k)
-    k=k+1
-
+fruits = ["apple", "banana", "cherry", "banana"]
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+    
 
 
 
