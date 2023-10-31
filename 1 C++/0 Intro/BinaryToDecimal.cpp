17a19a1159c1d12433cc295c75171ef9b8c4614e
Ravi Kumar <56071550+ravikumar1302@users.noreply.github.com>
2023-10-31 18:04:57 +0530

    31-10-2023

--- 1 C++/0 Intro/BinaryToDecimal.cpp
+++ 1 C++/0 Intro/BinaryToDecimal.cpp
@@ -6,11 +6,13 @@ int main()
 	int n;
 	cout<<"Enter n : ";
 	cin>>n;
-	int ans=0; int i=0;
+	int ans=0; 
+	int i=0;
 	while(n)
 	{
 		int digit=n%10;
-		if(digit==1)ans=ans+pow(2,i);
+		if(digit==1)
+			ans=ans+pow(2,i);
     n=n/10;
     i++;
 	}
