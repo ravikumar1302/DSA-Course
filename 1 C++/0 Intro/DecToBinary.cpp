d861080ac0a48a963eafac8d6d829db2a1f2b4fc
Ravi Kumar <56071550+ravikumar1302@users.noreply.github.com>
2023-11-06 20:17:04 +0530

    6-11-2023

--- 1 C++/0 Intro/DecToBinary.cpp
+++ 1 C++/0 Intro/DecToBinary.cpp
@@ -18,3 +18,25 @@ int main()
 	cout<<"Binary of n is: "<<ans;
 	return 0;
 }
+
+// Approach-2
+// Without using right shift
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int decimalNum, binaryNum=0, mul=1, rem;
+    cout<<"Enter the Decimal Number: ";
+    cin>>decimalNum;
+    while(decimalNum>0)
+    {
+        rem = decimalNum%2;
+        binaryNum = binaryNum+(rem*mul);
+        mul = mul*10;
+        decimalNum = decimalNum/2;
+    }
+    cout<<"\nEquivalent Binary Value: "<<binaryNum;
+    cout<<endl;
+    return 0;
+}
