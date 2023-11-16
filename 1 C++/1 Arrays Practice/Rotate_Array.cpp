bfca7c0cd6475f58bbb784058c830350b195aa09
Ravi Kumar <56071550+ravikumar1302@users.noreply.github.com>
2023-11-16 23:52:14 +0530

    16-11-2023

--- 1 C++/1 Arrays Practice/Rotate_Array.cpp
+++ 1 C++/1 Arrays Practice/Rotate_Array.cpp
@@ -1,9 +1,10 @@
 /*
 Rotate Array GFG
 Quick Left Rotation GFG
-Rotate Array LEetcode_189 : Right shift
+Rotate Array Leetcode_189 : Right shift
+Left Rotate an Array by One_Codestudio
 
-Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer.
 
 Example 1:
 Input : N = 5, D = 2, arr[] = {1,2,3,4,5}
@@ -14,50 +15,65 @@ Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
 */
 
+class Solution
+{
+public:
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(int arr[], int d, int n)
+    {
 
-class Solution{
-    public:
-    
-    //Function to rotate an array by d elements in counter-clockwise direction. 
-    void rotateArr(int arr[], int d, int n){
-        
         // Edge case : if d > n
-        d = d%n;
-        
+        d = d % n;
+
         // Try this method on Pen-Paper
-        reverse (arr + 0, arr + d);
-        reverse (arr + d, arr + n);
-        reverse (arr + 0, arr + n);
+        reverse(arr + 0, arr + d);
+        reverse(arr + d, arr + n);
+        reverse(arr + 0, arr + n);
     }
 };
-
-
 
 // Above code are for left shift
 
 // To right shift, code is
 
-void rotate(vector<int>& nums, int k) {
-        
-        int n = nums.size();
-        int d = k;
-        d = d%n;
+void rotate(vector<int> &nums, int k)
+{
 
-        d = n-d;    // The only diffrence, switching remaning terms left = switching k terms by right
+    int n = nums.size();
+    int d = k;
+    d = d % n;
 
-        // Try this method on Pen-Paper
-        reverse (nums.begin() + 0, nums.begin() + d);
-        reverse (nums.begin() + d, nums.begin() + n);
-        reverse (nums.begin() + 0, nums.begin() + n);
+    d = n - d; // The only diffrence, switching remaning terms left = switching k terms by right
+
+    // Try this method on Pen-Paper
+    reverse(nums.begin() + 0, nums.begin() + d);
+    reverse(nums.begin() + d, nums.begin() + n);
+    reverse(nums.begin() + 0, nums.begin() + n);
 }
 
 // Approach-2
 
-void rotate(vector<int>& nums, int k) {
-        vector<int> temp(nums.size());
-        for(int i=0;i<nums.size();i++)
-        {
-            temp[(i+k)%nums.size()] = nums[i];
-        }
-        nums=temp;
+void rotate(vector<int> &nums, int k)
+{
+    vector<int> temp(nums.size());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        temp[(i + k) % nums.size()] = nums[i];
+    }
+    nums = temp;
+}
+
+// For only one left shift
+vector<int> rotateArray(vector<int> &arr, int n)
+{
+    // Write your code here.
+
+    int first = arr[0];
+    for (int i = 0; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[n - 1] = first;
+
+    return arr;
 }
--- 1 C++/3 String/Valid_Palindrome_Leetcode_125.cpp
+++ 1 C++/3 String/Valid_Palindrome_Leetcode_125.cpp
@@ -11,33 +11,34 @@ Given a string s, return true if it is a palindrome, or false otherwise.
     Explanation: "amanaplanacanalpanama" is a palindrome.
 */
 
-class Solution {
-    private:
-   bool valid(char ch) 
-   {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) 
+class Solution
+{
+private:
+    bool valid(char ch)
+    {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
         {
             return 1;
         }
-        
+
         return 0;
     }
     char tolower(char ch)
     {
-        if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+        if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
             return ch;
         else
         {
-            return (ch-'A'+'a');
+            return (ch - 'A' + 'a');
         }
     }
     bool checkpalindrome(string a)
     {
-        int s=0;
-        int e=a.length()-1;
-        while(s<=e)
+        int s = 0;
+        int e = a.length() - 1;
+        while (s <= e)
         {
-            if(a[s]!=a[e])
+            if (a[s] != a[e])
                 return 0;
             else
             {
@@ -47,49 +48,82 @@ class Solution {
         }
         return 1;
     }
-    public:
-    bool isPalindrome(string s) {
-        string temp="";
-        for(int i=0;i<s.length();i++)
-            if(valid(s[i]))
-                temp.push_back(s[i]);      // Enterning alphabets in temp
-        
-        for(int i=0;i<temp.length();i++)
-            temp[i]=tolower(temp[i]);       //Converting everyting to lower case
-        
-        return checkpalindrome(temp);   //Checking palindrome
+
+public:
+    bool isPalindrome(string s)
+    {
+        string temp = "";
+        for (int i = 0; i < s.length(); i++)
+            if (valid(s[i]))
+                temp.push_back(s[i]); // Enterning alphabets in temp
+
+        for (int i = 0; i < temp.length(); i++)
+            temp[i] = tolower(temp[i]); // Converting everyting to lower case
+
+        return checkpalindrome(temp); // Checking palindrome
     }
 };
 
-
 // Same code as above, with alnum function used
 
-class Solution {
-    private:
-
+class Solution
+{
+private:
     bool checkpalindrome(string a)
     {
-        int s=0;
-        int e=a.length()-1;
-        while(s<=e)
+        int s = 0;
+        int e = a.length() - 1;
+        while (s <= e)
         {
-            if(a[s]!=a[e])
+            if (a[s] != a[e])
                 return 0;
-            
-            s++; e--;
+
+            s++;
+            e--;
         }
         return 1;
     }
-    public:
-    bool isPalindrome(string s) {
-        string temp="";
-        for(int i=0;i<s.length();i++)
-            if(isalnum(s[i]))
+
+public:
+    bool isPalindrome(string s)
+    {
+        string temp = "";
+        for (int i = 0; i < s.length(); i++)
+            if (isalnum(s[i]))
                 temp.push_back(s[i]);
-        
-        for(int i=0;i<temp.length();i++)
-            temp[i]=tolower(temp[i]);
-        
+
+        for (int i = 0; i < temp.length(); i++)
+            temp[i] = tolower(temp[i]);
+
         return checkpalindrome(temp);
+    }
+};
+
+// Method-3 : Using Recursion
+class Solution
+{
+public:
+    bool help(string &str, int n, int i)
+    {
+        if (i >= n / 2)
+            return true;
+        else if (str[i] != str[n - i - 1])
+            return false;
+
+        return help(str, n, i + 1);
+    }
+
+    bool isPalindrome(string s)
+    {
+        string temp = "";
+        for (int i = 0; i < s.length(); i++)
+            if (isalnum(s[i]))
+                temp.push_back(s[i]);
+
+        for (int i = 0; i < temp.length(); i++)
+            temp[i] = tolower(temp[i]);
+
+        int n = temp.length();
+        return help(temp, n, 0); // Using recurssion
     }
 };
