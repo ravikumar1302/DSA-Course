42238563cc44a78c59736349bebab3789a950137
Ravi Kumar <56071550+ravikumar1302@users.noreply.github.com>
2023-12-18 20:50:55 +0530

    18-12-2023

--- 1 C++/2 Searching and Sorting/Aggresive_cows_Codestudio.cpp
+++ 1 C++/2 Searching and Sorting/Aggresive_cows_Codestudio.cpp
@@ -1,25 +1,28 @@
 /*
 Aggresive_cows_Codestudio
-Given an array of length ‘N’, where each element denotes the position of a stall. 
-Now you have ‘N’ stalls and an integer ‘K’ which denotes the number of cows that are aggressive. 
-To prevent the cows from hurting each other, you need to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. 
+Aggressive Cows_GFG
+Magnetic Force Between Two Balls_Leetcode_1552
+
+Given an array of length ‘N’, where each element denotes the position of a stall.
+Now you have ‘N’ stalls and an integer ‘K’ which denotes the number of cows that are aggressive.
+To prevent the cows from hurting each other, you need to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible.
 Return the largest minimum distance.
 */
 
-bool ispossible(vector<int>&stalls,int k, int mid)
+bool ispossible(vector<int> &stalls, int k, int mid)
 {
-    int cowcount=1;
-    int lastpos=stalls[0];
-    for(int i=0;i<stalls.size();i++)
+    int cowcount = 1;
+    int lastpos = stalls[0];
+    for (int i = 0; i < stalls.size(); i++)
     {
-        if(stalls[i]-lastpos>=mid)
+        if (stalls[i] - lastpos >= mid)
         {
             cowcount++;
-            if(cowcount==k)
+            if (cowcount == k)
             {
                 return true;
             }
-            lastpos=stalls[i];
+            lastpos = stalls[i];
         }
     }
     return false;
@@ -27,24 +30,24 @@ bool ispossible(vector<int>&stalls,int k, int mid)
 
 int aggressiveCows(vector<int> &stalls, int k)
 {
-    sort(stalls.begin(),stalls.end());
-    int s=0;
-    int maxi=-1;
-    for(int i=0;i<stalls.size();i++)
-        maxi=max(maxi,stalls[i]);
-    int e=maxi;
-    int ans=-1;
-    int mid=s+(e-s)/2;
-    while(s<=e)
+    sort(stalls.begin(), stalls.end());
+    int s = 0;
+    int maxi = -1;
+    for (int i = 0; i < stalls.size(); i++)
+        maxi = max(maxi, stalls[i]);
+    int e = maxi;
+    int ans = -1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
     {
-        if(ispossible(stalls,k,mid))
+        if (ispossible(stalls, k, mid))
         {
-            ans=mid;
-            s=mid+1;
+            ans = mid;
+            s = mid + 1;
         }
         else
-           e=mid-1;
-        mid=s+(e-s)/2;
+            e = mid - 1;
+        mid = s + (e - s) / 2;
     }
     return ans;
 }
