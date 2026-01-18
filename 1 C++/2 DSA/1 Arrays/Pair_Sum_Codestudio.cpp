684a53f94e062c4fe5e0a43342a101061b9f0697
Ravi Kumar <ravibansal130201@gmail.com>
2026-01-18 23:09:24 +0530

    saving

--- 1 C++/2 DSA/1 Arrays/Pair_Sum_Codestudio.cpp
+++ 1 C++/2 DSA/1 Arrays/Pair_Sum_Codestudio.cpp
@@ -1,50 +1,47 @@
 /*
 Pair_Sum_Codestudio
 
-You are given an integer array 'ARR' of size 'N' and an integer 'S'. 
+You are given an integer array 'ARR' of size 'N' and an integer 'S'.
 Your task is to return the list of all pairs of elements such that each sum of elements of each pair equals 'S'.
 */
 
-#include <bits/stdc++.h> 
-vector<vector<int>> pairSum(vector<int> &arr, int s){
-   // Write your code here.
-	vector<vector<int>> ans;
-	for(int i=0;i<arr.size();i++)
-	{
-		for(int j=i+1;j<arr.size();j++)
-		{
-			if(arr[i]+arr[j]==s)
-			{
-				vector <int> temp;
-				temp.push_back(min(arr[i],arr[j]));
-				temp.push_back(max(arr[i],arr[j]));
-				ans.push_back(temp);
-			}
-		}
-	}
-	sort(ans.begin(),ans.end());
-	return ans;
+#include <bits/stdc++.h>
+vector<vector<int>> pairSum(vector<int> &arr, int s)
+{
+    vector<vector<int>> ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i] + arr[j] == s)
+            {
+                vector<int> temp;
+                temp.push_back(min(arr[i], arr[j]));
+                temp.push_back(max(arr[i], arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
 }
 // Time : O(N*N)
 
-
-
 // Approach-2
-#include <bits/stdc++.h> 
-#include <bits/stdc++.h> 
-vector<vector<int>> pairSum(vector<int> &arr, int s){
-   
+#include <bits/stdc++.h>
+vector<vector<int>> pairSum(vector<int> &arr, int s)
+{
     vector<vector<int>> ans;
     int n = arr.size();
     sort(arr.begin(), arr.end());
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         int temp = arr[i];
-        for(int j=n-1; j>=0 && j>i; j--)
+        for (int j = n - 1; j >= 0 && j > i; j--)
         {
-            if(temp + arr[j] == s)
-                ans.push_back( {temp, arr[j]} );
-            else if(temp + arr[j] > s)
+            if (temp + arr[j] == s)
+                ans.push_back({temp, arr[j]});
+            else if (temp + arr[j] > s)
                 continue;
             else
                 break;
--- 1 C++/2 DSA/1 Arrays/Sort_012_Codestudio.cpp
+++ 1 C++/2 DSA/1 Arrays/Sort_012_Codestudio.cpp
@@ -2,6 +2,13 @@
 // Sort_012_Codestudio
 // Sort An Array of 0s, 1s and 2s_Codestudio
 
+/*
+You have been given an integer array/list(ARR) of size 'N'. It only contains 0s, 1s and 2s. Write a solution to sort this array/list.
+
+Note :
+Try to solve the problem in 'Single Scan'. ' Single Scan' refers to iterating over the array/list just once or to put it in other words, you will be visiting each element in the array/list just once.
+*/
+
 #include <bits/stdc++.h>
 void sort012(int *arr, int n)
 {
