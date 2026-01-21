0d0915592cef3a7788b52905d809aba2ef0c5d82
Ravi Kumar <ravibansal130201@gmail.com>
2026-01-21 21:02:35 +0530

    updating Binary search theory details

--- 1 C++/2 DSA/2 Searching and Sorting/Binary_search_code.cpp
+++ 1 C++/2 DSA/2 Searching and Sorting/Binary_search_code.cpp
@@ -1,60 +1,78 @@
-#include<iostream>
+// T.C.: O(log n)
+// S.C.: O(log n)
+
+// Note: Binary search only works on Sorted data
+/*
+STL way of Binary Search:
+    1. Ascending: binary_search(v.begin(), v.end(), key)
+    2. Descending:binary_search(v.begin(), v.end(), key, greater<int>())
+*/
+
+#include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size, int key) {
+int binarySearch(int arr[], int size, int key)
+{
 
     int start = 0;
-    int end = size-1;
+    int end = size - 1;
 
-    int mid = start + (end-start)/2;
+    int mid = start + (end - start) / 2;
 
-    while(start <= end) {
+    while (start <= end)
+    {
 
-        if(arr[mid] == key) {
+        if (arr[mid] == key)
+        {
             return mid;
         }
 
-        //go to right wala part
-        if(key > arr[mid]) {
+        // go to right wala part
+        if (key > arr[mid])
+        {
             start = mid + 1;
         }
-        else{ //key < arr[mid]
+        else
+        { // key < arr[mid]
             end = mid - 1;
         }
 
-        mid = start + (end-start)/2;
+        mid = start + (end - start) / 2;
     }
-    
+
     return -1;
 }
 
+int main()
+{
 
-int main() { 
-
-    int even[6] = {2,4,6,8,12,18};
+    int even[6] = {2, 4, 6, 8, 12, 18};
 
     int Index = binarySearch(even, 6, 6);
 
-    cout << " Index of 6 is " <<Index << endl;
-
+    cout << " Index of 6 is " << Index << endl;
 
     return 0;
 }
 
-int findPeak(int arr[], int n) {
+int findPeak(int arr[], int n)
+{
 
-    int s =0, e = n-1;
-    int mid = s + (e-s)/2;
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
 
-    while(s<e) {
-        //cout<<" s " << s <<" e " << e << endl;
-        if(arr[mid] < arr[mid+1]){
-            s = mid+1; 
+    while (s < e)
+    {
+        // cout<<" s " << s <<" e " << e << endl;
+        if (arr[mid] < arr[mid + 1])
+        {
+            s = mid + 1;
         }
-        else{
+        else
+        {
             e = mid;
         }
-        mid = s + (e-s)/2;
+        mid = s + (e - s) / 2;
     }
     return s;
 }
